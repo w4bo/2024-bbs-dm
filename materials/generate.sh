fdf625d641f580fa0093bd98f19a660dd6db6be0
mfrancia <matteo.francia2@studio.unibo.it>
2024-04-09 14:39:36 +0200

    add ml slides

---
+++ materials/generate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -exo
+# for FILE in *.ipynb; do 
+for FILE in "01-DataPreprocessing.ipynb" "02-MachineLearning.ipynb"; do 
+    echo "Processing $FILE file...";
+    filename=$(basename -- "$FILE")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    jupyter nbconvert --execute --to notebook --inplace "$FILE"
+    jupyter nbconvert "$FILE" --to slides
+    sed -i 's+</head>+<link rel="stylesheet" href="./mytheme.css" id="theme"></head>+g' "$filename.slides.html"
+done
